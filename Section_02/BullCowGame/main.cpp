7f006046689f67177e9959c58719ed3a0a1ec9a7
Michael Craig <m_craig@baylor.edu>
2018-09-15 15:22:42 -0400

    getting input from player

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
+	// introduce game
 	constexpr int WORD_LENGTH = 9;
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of?\n";
+
+	// get guess from the player
+	cout << "Enter your guess: ";
+	string Guess = "";
+	cin >> Guess;
+
+	// repeat the guess back to player
+	cout << "Your guess was: " << Guess << endl;
+
+	// get guess from the player
+	cout << "Enter your guess: ";
+	cin >> Guess;
+
+	// repeat the guess back to player
+	cout << "Your guess was: " << Guess << endl;
+
+	cout << endl;
+	return 0;
 }
